6c78829df770606290412fdac9ca33328e9b6a0d
LLLLLLs <1181947970@qq.com>
2017-05-18 23:59:42 +0800

    增加一点界面……

--- CommodityInfoManagement/Form1.Designer.cs
+++ CommodityInfoManagement/Form1.Designer.cs
@@ -29,40 +29,53 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.high_weight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.low_weight = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.hign_price = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.low_price = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.sign_in = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 21);
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("宋体", 10F);
+            this.button1.Location = new System.Drawing.Point(35, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(146, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "商品搜索";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(109, 21);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "我的商品";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(187, 68);
+            this.textBox1.Location = new System.Drawing.Point(25, 22);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(173, 21);
+            this.textBox1.Size = new System.Drawing.Size(173, 23);
             this.textBox1.TabIndex = 2;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(68, 66);
+            this.button3.Location = new System.Drawing.Point(227, 22);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -72,37 +85,226 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(387, 68);
+            this.comboBox1.Location = new System.Drawing.Point(25, 68);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(109, 20);
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 4;
             this.comboBox1.Text = "选择商品类别...";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 10F);
+            this.groupBox1.Location = new System.Drawing.Point(35, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(334, 109);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "搜索";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.high_weight);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.low_weight);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.hign_price);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.low_price);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Font = new System.Drawing.Font("宋体", 10F);
+            this.groupBox2.Location = new System.Drawing.Point(420, 59);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(363, 109);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "筛选";
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(305, 22);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 67);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "确定";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // high_weight
+            // 
+            this.high_weight.Location = new System.Drawing.Point(230, 66);
+            this.high_weight.Name = "high_weight";
+            this.high_weight.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.high_weight.Size = new System.Drawing.Size(57, 23);
+            this.high_weight.TabIndex = 9;
+            this.high_weight.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 14);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "——";
+            // 
+            // low_weight
+            // 
+            this.low_weight.Location = new System.Drawing.Point(126, 66);
+            this.low_weight.Name = "low_weight";
+            this.low_weight.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.low_weight.Size = new System.Drawing.Size(57, 23);
+            this.low_weight.TabIndex = 8;
+            this.low_weight.Text = "0";
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(20, 66);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 23);
+            this.button5.TabIndex = 999;
+            this.button5.Text = "重量(KG)：";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // hign_price
+            // 
+            this.hign_price.Location = new System.Drawing.Point(230, 24);
+            this.hign_price.Name = "hign_price";
+            this.hign_price.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.hign_price.Size = new System.Drawing.Size(57, 23);
+            this.hign_price.TabIndex = 7;
+            this.hign_price.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 14);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "——";
+            // 
+            // low_price
+            // 
+            this.low_price.Location = new System.Drawing.Point(126, 24);
+            this.low_price.Name = "low_price";
+            this.low_price.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.low_price.Size = new System.Drawing.Size(57, 23);
+            this.low_price.TabIndex = 6;
+            this.low_price.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(20, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 999;
+            this.button4.Text = "价格(￥)：";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Font = new System.Drawing.Font("宋体", 10F);
+            this.groupBox3.Location = new System.Drawing.Point(35, 189);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(747, 387);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "搜索结果";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(737, 357);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("宋体", 10F);
+            this.button7.Location = new System.Drawing.Point(547, 591);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(71, 24);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "购买";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("宋体", 10F);
+            this.button8.Location = new System.Drawing.Point(632, 591);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(145, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "管理我的商品";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // sign_in
+            // 
+            this.sign_in.Font = new System.Drawing.Font("宋体", 10F);
+            this.sign_in.Location = new System.Drawing.Point(700, 12);
+            this.sign_in.Name = "sign_in";
+            this.sign_in.Size = new System.Drawing.Size(76, 27);
+            this.sign_in.TabIndex = 11;
+            this.sign_in.Text = "登陆";
+            this.sign_in.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1295, 628);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(834, 627);
+            this.Controls.Add(this.sign_in);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Name = "Form1";
             this.Text = "SHOP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox high_weight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox low_weight;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox hign_price;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox low_price;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button sign_in;
     }
 }
 
