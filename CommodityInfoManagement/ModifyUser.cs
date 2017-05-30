173bb7f4cc62cfb956020ef302facd2fae5e13b0
Xiangyi Meng <xymeng16@gmail.com>
2017-05-30 15:53:53 +0800

    ModifyUser done.

--- CommodityInfoManagement/ModifyUser.cs
+++ CommodityInfoManagement/ModifyUser.cs
@@ -12,9 +12,54 @@ namespace CommodityInfoManagement
 {
     public partial class ModifyUser : Form
     {
-        public ModifyUser()
+        private bool isAdd;
+        public ModifyUser(User user)
         {
             InitializeComponent();
+            if(user != null)
+            {
+                isAdd = false;
+                Text = "修改用户";
+                username.Text = user.Username;
+                username.Enabled = false;
+                password.Enabled = false;
+                category.Items.AddRange(new string[] { "消费者", "卖家" });
+                if(user.Role == "超级管理员")
+                {
+                    category.Items.Add("管理员");
+                }
+            }
+            else
+            {
+                isAdd = true;
+                Text = "添加用户";
+                category.Items.AddRange(new string[] { "消费者", "卖家" });
+                if (user.Role == "超级管理员")
+                {
+                    category.Items.Add("管理员");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sqlCommand;
+            if(isAdd)
+            {
+                sqlCommand = String.Format("insert into comm_user values(null, '{1}', sha1('{2}'), now());", 
+                    username.Text, password.Text, User.RoleStr[category.Text]);
+            }
+            else
+            {
+                sqlCommand = String.Format("update comm_user_role set role_id=(select role_id from comm_role" +
+                    " where role_name='{1}') where user_id=(select user_id from comm_user where username" +
+                    "={2});", User.RoleStr[category.Text], username.Text);
+            }
+            using (MySqlAdapter adapter = new MySqlAdapter())
+            {
+                adapter.ExecuteNonQuery(sqlCommand);
+            }
+            MessageBox.Show("用户" + (isAdd ? "添加" : "修改") + "成功！");
         }
     }
 }
--- CommodityInfoManagement/User.cs
+++ CommodityInfoManagement/User.cs
@@ -1,24 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CommodityInfoManagement
 {
-    public enum USER_ROLE
-    {
-        CUSTOMER,
-        SHOPKEEPER,
-        ADMIN,
-        SUPERUSER
-    };
     public class User
     {
         private string __username;
         private string __role;
         private DateTime __datetime;
-
+        public static NameValueCollection RoleStr = new NameValueCollection();
+        
         public string Username { get => __username; set => __username = value; }
         public string Role { get => __role; set => __role = value; }
         public DateTime Datetime { get => __datetime; set => __datetime = value; }
@@ -28,6 +23,10 @@ namespace CommodityInfoManagement
             Username = username;
             Role = role;
             Datetime = datetime;
+            RoleStr.Add("消费者", "CUSTOMER");
+            RoleStr.Add("卖家", "SHOPKEEPER");
+            RoleStr.Add("管理员", "ADMIN");
+            RoleStr.Add("超级管理员", "SUPER_USER");
         }
     }
 }
